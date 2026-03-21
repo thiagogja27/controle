114475759f1c81e8b8c2f35d3133b022a99b0313
thiago.viaembratelgja <thiago.viaembratelgja@gmail.com>
2026-03-21 15:41:13 +0000

    Force Node.js 18.x and remove OpenSSL workarounds

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import withSerwistInit from "@serwist/next";
 import type { NextConfig } from "next";
 
-// Adiciona a flag para compatibilidade com OpenSSL
-process.env.NODE_OPTIONS = "--openssl-legacy-provider";
-
 const withSerwist = withSerwistInit({
   swSrc: "app/sw.ts",
   swDest: "public/sw.js",
