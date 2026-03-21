d8d579c6271eb40d1ce88858265dd1852b19a82c
thiago.viaembratelgja <thiago.viaembratelgja@gmail.com>
2026-03-21 23:47:43 +0000

    fix: Corrige a configuração do Firebase no lado do cliente

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,18 +1,19 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { Auth, getAuth } from "firebase/auth";
 import { Database, getDatabase } from "firebase/database";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA8DbMSU96gaxqFYKDfC0fhakhwly6Iv78",
+  authDomain: "controle-diversos.firebaseapp.com",
+  databaseURL: "https://controle-diversos-default-rtdb.firebaseio.com",
+  projectId: "controle-diversos",
+  storageBucket: "controle-diversos.appspot.com",
+  messagingSenderId: "531212269985",
+  appId: "1:531212269985:web:ebb9f8decc7f28573eb3e0"
 };
 
-// Initialize Firebase only if we have an API Key (prevents build errors)
 let app;
 try {
     if (firebaseConfig.apiKey) {
